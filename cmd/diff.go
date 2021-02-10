dab3d676f1259654a197811625d5627ff4a1aaa7
Derek Smith <dsmith@goodwaygroup.com>
2021-02-10 13:15:57 -0600

    chore(deps): update module r3labs/diff to v2

--- cmd/diff.go
+++ cmd/diff.go
@@ -7,7 +7,7 @@ import (
 	"github.com/clok/kemba"
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/logrusorgru/aurora/v3"
-	"github.com/r3labs/diff"
+	"github.com/r3labs/diff/v2"
 	"github.com/sergi/go-diff/diffmatchpatch"
 	"github.com/urfave/cli/v2"
 	"os"
